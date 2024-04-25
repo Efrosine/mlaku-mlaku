f76a794ede144d0926998271f4e8125debf09870
Efrosine <efrosinegames@gmail.com>
2024-04-25 08:30:56 +0700

    commit tambahan yang mengubah main

--- lib/features/presentation/widgets/additional_feature_widget.dart
+++ lib/features/presentation/widgets/additional_feature_widget.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 
 class AdditionalFeatureWidget extends StatelessWidget {
@@ -8,33 +10,65 @@ class AdditionalFeatureWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SizedBox.fromSize(
-      size: Size.fromHeight(190),
+      size: Size.fromHeight(210),
       child: GridView.builder(
-          itemCount: 10,
+          itemCount: listItems.length,
           padding: EdgeInsets.symmetric(horizontal: 16),
           scrollDirection: Axis.horizontal,
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
               maxCrossAxisExtent: 110,
               mainAxisExtent: 64,
-              crossAxisSpacing: 24,
-              mainAxisSpacing: 24),
-          itemBuilder: (context, index) => Container(
-                // color: Colors.red,
-                child:
-                    Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-                  CircleAvatar(
-                    backgroundColor: Colors.blueGrey[50],
-                    radius: 24,
-                    child: Icon(
-                      Icons.directions_bus,
-                    ),
-                  ),
-                  Text(
-                    'Bus & Travel',
-                    style: TextStyle(fontSize: 12),
-                  )
-                ]),
-              )),
+              crossAxisSpacing: 48,
+              mainAxisSpacing: 38),
+          itemBuilder: (context, index) =>
+              AddtitionalFeatureItemWidget(items: listItems[index])),
+    );
+  }
+}
+
+List<AddsItems> listItems = [
+  AddsItems(iconData: Icons.directions_bus, name: 'Bus & Travel'),
+  AddsItems(iconData: Icons.lte_mobiledata_sharp, name: 'Pulsa'),
+  AddsItems(iconData: Icons.electric_bike, name: 'Eco Bike'),
+  AddsItems(iconData: Icons.electric_bolt, name: 'Listrik'),
+  AddsItems(iconData: Icons.car_rental, name: 'Taxi'),
+  AddsItems(iconData: Icons.directions_bus, name: 'Bus & Travel'),
+  AddsItems(iconData: Icons.lte_mobiledata_sharp, name: 'Pulsa'),
+  AddsItems(iconData: Icons.lte_mobiledata_sharp, name: 'Pulsa'),
+  AddsItems(iconData: Icons.lte_mobiledata_sharp, name: 'Pulsa'),
+];
+
+class AddsItems {
+  final IconData iconData;
+  final String name;
+
+  AddsItems({required this.iconData, required this.name});
+}
+
+class AddtitionalFeatureItemWidget extends StatelessWidget {
+  const AddtitionalFeatureItemWidget({super.key, required this.items});
+
+  final AddsItems items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // color: Colors.red,
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        CircleAvatar(
+          backgroundColor: Colors.blueGrey[50],
+          radius: 32,
+          child: Icon(
+            items.iconData,
+            size: 28,
+          ),
+        ),
+        Text(
+          items.name,
+          style: TextStyle(fontSize: 12),
+        )
+      ]),
     );
   }
 }
